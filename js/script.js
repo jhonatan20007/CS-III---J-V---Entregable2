a84bd1197f50942fd1fdcd8de7a47cf4b4edc94a
jhonatan20007 <86896817+jhonatan20007@users.noreply.github.com>
2022-09-10 02:30:50 -0500

    .
    .

--- js/script.js
+++ js/script.js
@@ -1,38 +1,92 @@
-
-var i =0;
+var i = 0;
+option = "option";
 $(function () {
-  $("#btn1").click(function (e) {
-
-  
-  
-  });
-  let tipo = ["1", "2","3"];
-  let valores = ["35000", "75000","18000"];
+  $("#btn1").click(function (e) {});
+  let tipo = ["1", "2", "3"];
+  let valores = ["35000", "75000", "18000"];
   for (let i = 0; i < 3; i++) {
-    llenarTable(tipo,valores);
+    llenarTable(tipo, valores);
   }
+
+  $("#btnagregar").click(function (event) {
+    agregar(1);
+  });
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
 
 });
 
-function llenarTable(tipo,valores) {
+function llenarTable(tipo, valores) {
   var row = $("<tr />");
   $("#table tbody").append(row);
-  row.append($("<td>" + "" + "</td>"));
+  row.append($('<td id="btnagregar" class="icon-user-plus">' + "" + "</td>"));
   row.append($("<td>" + tipo[i] + "</td>"));
   row.append($("<td>" + valores[i] + "</td>"));
   row.append(
     $(
       "<td>" +
-        '<select name="option" id="option">' +
-        "<option></option>" +
-        "<option>1</option>" +
-        "<option>2</option>" +
-        "<option>3</option>" +
+        '<select name="option" id="' +
+        option +
+        i +
+        '">' +
+        "<option ></option>" +
+        '<option value="1">1</option>' +
+        '<option value="2">2</option>' +
+        '<option value="3">3</option>' +
         "</select>" +
         "</td>"
     )
   );
   i++;
+}
+
+function agregar(op) {
+  // if(op == 1){
+  //     $("#table").dblclick();
+  // }
+  $("#table").on("dblclick", "tbody tr", function (event) {
+    let iva, cantidad, valor, total, tipo;
+    var $row = $(this).closest("tr");
+    var $tds = $row.find("td");
+    // var vacio = $tds[0].innerText;
+    tipo = $tds[1].innerText;
+    valor = $tds[2].innerText;
+    // var cantidad = $tds[3].innerText;
+    // var cantidad = $("#option").val();  $(this).children("option:selected").val();
+    // var selectedOption = this.options[select.selectedIndex];
+    //necesito tamar el campor de cantidad que es
+    //debugger;
+    switch (tipo) {
+      case "1":
+       // alert("hola")
+        cantidad = $("#option0").val();
+        valor = (valor * cantidad).toString();
+        iva = ((valor * 16) / 100).toString();
+        total = (parseInt(iva) + parseInt(valor)).toString();
+        break;
+      case "2":
+        cantidad = $("#option1").val();
+        valor = (valor * cantidad).toString();
+        iva = ((valor * 16) / 100).toString();
+        total = (parseInt(iva) + parseInt(valor)).toString();
+
+        break;
+      case "3":
+        cantidad = $("#option2").val();
+        valor = (valor * cantidad).toString();
+        iva = ((valor * 16) / 100).toString();
+        total = (parseInt(iva) + parseInt(valor)).toString();
+
+        break;
+    }
+
+    $("#valor").text(valor);
+    $("#iva").text(iva);
+    $("#total").text(total);
+    console.log(tipo+" "+valor + " " + iva + " " + total + " " + cantidad);
+
+  });
 }
 
 // funcion para Cargar Provincias al campo <select>
